45f5aebce57c45069f6af70d150fae18d41b72b8
jean <forconijean@gmail.com>
2022-12-11 21:51:40 +0100

    modif pour le controle des parametre d'entrée

--- CreateWebSite.sh
+++ CreateWebSite.sh
@@ -4,43 +4,16 @@ page="$1"
 title="$2"
 folder="$3"
 
-controlOption() {
-    while getopts ":hn:" option; do
-        case $option in
-        h) # display Help
-            Help
-            exit
-            ;;
-        n) # Enter a name
-            Name=$OPTARG ;;
-        \?) # Invalid option
-            echo "Error: Invalid option"
-            Help
-            exit
-            ;;
-        esac
-    done
-}
-
-Help() {
-    echo "Add description of the script functions here."
-    echo
-    echo "Syntax: CreateWebSite \"nom du fichier" "titre" "chemin des images"\"
-    echo "options:"
-    echo "g     Print the GPL license notification."
-    echo "h     Print this Help."
-    echo "v     Verbose mode."
-    echo "V     Print software version and exit."
-    echo
-}
-
-Name="world"
-
 controlParametre() {
 
     if [ $# = 3 ]; then
         return 1
-    elif [ "$1" = "" ]; then
+    elif [ "$#" -gt "3" ]; then
+        echo "trop de paramètres"
+        help
+        exit
+    fi
+    if [ "$1" = "" ]; then
         echo -e "Enter new web page name ... \c"
         read -r "page"
     else
@@ -57,7 +30,10 @@ controlParametre() {
         read -r "folder"
     else
         folder="$3"
+
     fi
+
+    return 1
 
 }
 
@@ -70,46 +46,90 @@ controlFichier() {
 
 }
 
-controlOption "$@"
+makePost() {
+
+    for item in ./"$1"/Picture/*; do
+        if [[ "${item}" =~ \.(j?p(e|n)?g|webp) ]]; then
+
+            nomFichier="${item##*/}"
+            nomFichier=$(echo "$nomFichier" | cut -f 1 -d '.')
+
+            controlFichier "$nomFichier" "./Picture/"
+
+            titre=$(grep titre ./Picture/"${nomFichier}".txt | cut -d ":" -f 2)
+            description=$(grep description ./Picture/"${nomFichier}".txt | cut -d ":" -f 2)
+            lien=$(grep lien ./Picture/"${nomFichier}".txt | cut -d ":" -f 2)
+
+            cp ../src/picture.php ./Picture
+            mv ./Picture/picture.php ./Picture/"${nomFichier}".php
+
+            cheminPic="./Picture/${nomFichier}.jpeg"
+
+            sed -i '' "s/titlePicture/${nomFichier}/" Picture/"${nomFichier}".php
+            sed -i '' "s/titleCard/${titre}/" ./Picture/"${nomFichier}".php
+            sed -i '' "s@folder@${cheminPic}@" ./Picture/"${nomFichier}".php
+            sed -i '' "s/description/${description}/" ./Picture/"${nomFichier}".php
+            sed -i '' "s/lien/${lien}/" ./Picture/"${nomFichier}".php
+
+            cat ./Picture/"$nomFichier".php >>home.php
+        fi
+
+    done
+}
+
+makeAll() {
+
+    mkdir "$title"
+    cd "$title" || exit
+
+    cp -R ../src/* ./
+    sed -i '' "s/HOME/$page/" home.php
+    sed -i '' "s/HOME/$page/" header.php
+    mv -f images Picture
+
+    makePost ./
+
+    cd ..
+    echo "$folder"
+    mv "$title" "$folder"
+}
+
+Help() {
+    echo "Add description of the script functions here."
+    echo
+    echo "Syntax: CreateWebSite \"nom du fichier" "titre" "chemin des images"\"
+    echo "options:"
+    echo "h     Print this Help."
+    echo "r     Reload the description of the post"
+    echo "Make command like this: ./CreateWebSite.sh page titre ./test   "
+}
+
+controlOption() {
+    while getopts ":hr:" option; do
+        case $option in
+        h) # display Help
+            Help
+            exit
+            ;;
+        r)
+            makePost /test/titre/
+            exit
+            ;;
+        \?) # Invalid option
+            echo "Error: Invalid option"
+            Help
+            exit
+            ;;
+
+        esac
+
+    done
+
+    makeAll "$@"
+    exit
+}
+
 controlParametre "$@"
 
-mkdir "$title"
-cd "$title" || exit
+controlOption "$@"
 
-cp -R ../src/* ./
-sed -i '' "s/HOME/$page/" home.php
-sed -i '' "s/HOME/$page/" header.php
-mv -f images Picture
-
-for item in ./Picture/*; do
-    if [[ "${item}" =~ \.(j?p(e|n)?g|webp) ]]; then
-
-        nomFichier="${item##*/}"
-        nomFichier=$(echo "$nomFichier" | cut -f 1 -d '.')
-
-        controlFichier "$nomFichier" "./Picture/"
-     
-        titre=$(grep titre ./Picture/"${nomFichier}".txt | cut -d ":" -f 2 )
-        description=$(grep description ./Picture/"${nomFichier}".txt | cut -d ":" -f 2 )
-        lien=$(grep lien ./Picture/"${nomFichier}".txt | cut -d ":" -f 2 )
-       
-        cp ../src/picture.php ./Picture
-        mv ./Picture/picture.php ./Picture/"${nomFichier}".php
-
-        cheminPic="./Picture/${nomFichier}.jpeg"
-
-        sed -i '' "s/titlePicture/${nomFichier}/" Picture/"${nomFichier}".php
-        sed -i '' "s/titleCard/${titre}/" ./Picture/"${nomFichier}".php
-        sed -i '' "s@folder@${cheminPic}@" ./Picture/"${nomFichier}".php
-        sed -i '' "s/description/${description}/" ./Picture/"${nomFichier}".php
-        sed -i '' "s/lien/${lien}/" ./Picture/"${nomFichier}".php
-
-        cat ./Picture/"$nomFichier".php >>home.php
-    fi
-
-done
-
-cd ..
-
-mv  "$title" /"$3"/
-#sed "s/title/${titlePicture}/" ../src/home.php > home.php
